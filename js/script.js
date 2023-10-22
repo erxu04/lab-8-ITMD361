5aaae4aeb0c8c48b1ff29aaee6d2152aa3e8ae09
Emily Xu <emojibear123@gmail.com>
2023-10-21 21:36:45 -0500

    changed order of console log statments

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 function init(){
 	var button = document.getElementById("entrybutton");
-  var result = document.getElementById("textoutput").innerHTML; //stores copy of html element's content into variable result
+  var result = document.getElementById("textoutput").innerHTML; //stores copy of html element's content into variable result for console printing
   
   function myEventText(){
 		var textInput = document.getElementById("entryinput").value;
   	alert("Emily Xu: " + textInput);
-    console.log("wejfewfoe");
-    console.log("result: " + result);
+    console.log("Alert executed!");
     console.log("textInput: " + textInput);
+    console.log("initial result: " + result);
     /* result = textInput; */	//this doesn't change the actual html content, just the value stored in result variable which is not tied to HTML
     document.getElementById("textoutput").innerHTML = textInput; //directly affects HTML element
     
